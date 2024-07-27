ddabb49cb5ddd79961aec96284a6203d9c1d4a84
Josue Leiva Mora <leivajos1499@gmail.com>
2024-07-26 19:16:42 -0600

    Update UserRepository.java
    Fixing stashed changes bug

--- src/main/java/com/example/igniteu/Repository/UserRepository.java
+++ src/main/java/com/example/igniteu/Repository/UserRepository.java
@@ -1,24 +1,13 @@
 package com.example.igniteu.Repository;
 
-<<<<<<< Updated upstream
-=======
 import java.util.List;
 import java.util.Optional;
-
->>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.igniteu.models.Usertable;
 
 public interface UserRepository extends JpaRepository<Usertable, Integer> {
 
     public Usertable findByCorreo(String correo);
-<<<<<<< Updated upstream
-}
-=======
-    
     Optional<Usertable> findByUsername(String username);
-
     public List<Usertable> findByUsernameContainingIgnoreCase(String nombre);
 }
->>>>>>> Stashed changes
